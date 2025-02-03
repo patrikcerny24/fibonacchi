7aa4c38d0b299ccb37e8486dddd9dbcc4240a949
patrikcerny24 <pata.cerny@centrum.cz>
2025-02-03 15:20:52 +0100

    hvezdicky 2

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-// Funkce pro generování Fibonacciho posloupnosti
 function generateFibonacci(n) {
-
+    let fibonacci = document.getElementById('fibonacci');
+    let a = 1, b = 1, temp;
+    let i = 0;
+    while (i <= n) {
+     const mesic = document.createElement('div');
+     mesic.classList.add('rabbit');
+     let stars = '';
+     let j =0;
+     while (j<a) {
+        stars += '*';
+        j++;
+     }
+     mesic.innerHTML = stars;
+     fibonacci.appendChild(mesic);
+     temp = a+b;
+     a=b;
+     b=temp;
+     i++;
+    }
 }
 
-
-// Spuštění generování Fibonacciho posloupnosti s 6 prvky
-const n = 6;
+const n = 10;
 generateFibonacci(n);
